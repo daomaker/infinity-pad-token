96760b3de1feb0af3f618271fd5a3d85dfbf7f46
vmatskiv <volodymyr.matskiv@devrecords.com>
2021-06-16 17:19:46 +0300

    feat: added token contract

---
+++ contracts/tokens/InfinityPadToken.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+
+contract InfinityPadToken is Ownable, ERC20Burnable {
+  constructor (string memory name, string memory symbol, uint256 amount)
+    ERC20(name, symbol) {
+      _mint(_msgSender(), amount);
+  }
+
+  /**
+   * @dev Creates `amount` tokens and assigns them to `msg.sender`, increasing
+   * the total supply.
+   *
+   * Requirements
+   *
+   * - `msg.sender` must be the token owner
+   */
+  function mint(uint256 amount) public onlyOwner returns (bool) {
+    _mint(_msgSender(), amount);
+    return true;
+  }
+}
